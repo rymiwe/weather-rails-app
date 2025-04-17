c4ac7982a6efa57e8d6382a8224bf97f3b98ea03
ryan.weiss <rymiwe@gmail.com>
2025-04-16 21:04:19 -0700

    Add helper to alias 'clear-day' to 'day' and 'clear-night' to 'night' for Masters-Tempestacons weather icons. Use helper in forecast card view.

--- app/helpers/forecasts_helper.rb
+++ app/helpers/forecasts_helper.rb
@@ -1,2 +1,9 @@
 module ForecastsHelper
+  def weather_icon_filename(icon_name)
+    aliases = {
+      "clear-day" => "day",
+      "clear-night" => "night"
+    }
+    aliases[icon_name] || icon_name
+  end
 end
