9337145fb7be4af493d5c07486e4406ce02cfbaf
ryan.weiss <rymiwe@gmail.com>
2025-04-16 21:18:21 -0700

    Comprehensive mapping: Map all Pirate Weather icon classes (standard + expanded) to Masters-Tempestacons equivalents in forecasts_helper.rb.

--- app/helpers/forecasts_helper.rb
+++ app/helpers/forecasts_helper.rb
@@ -1,10 +1,46 @@
 module ForecastsHelper
   def weather_icon_filename(icon_name)
     aliases = {
+      # Standard
       "clear-day" => "day",
       "clear-night" => "night",
       "partly-cloudy-day" => "cloudy-day",
-      "partly-cloudy-night" => "cloudy-night"
+      "partly-cloudy-night" => "cloudy-night",
+      "cloudy" => "cloudy",
+      "rain" => "rain",
+      "snow" => "snow",
+      "sleet" => "sleet",
+      "wind" => "wind",
+      "fog" => "fog",
+      "thunderstorm" => "thunderstorm",
+      "hail" => "hail",
+      "mixed" => "precipitation",
+      "none" => "none",
+      # Expanded
+      "mostly-clear-day" => "day",
+      "mostly-clear-night" => "night",
+      "mostly-cloudy-day" => "cloudy-day",
+      "mostly-cloudy-night" => "cloudy-night",
+      "possible-rain-day" => "rain",
+      "possible-rain-night" => "rain",
+      "possible-snow-day" => "snow",
+      "possible-snow-night" => "snow",
+      "possible-sleet-day" => "sleet",
+      "possible-sleet-night" => "sleet",
+      "possible-precipitation-day" => "precipitation",
+      "possible-precipitation-night" => "precipitation",
+      "precipitation" => "precipitation",
+      "drizzle" => "drizzle",
+      "light-rain" => "light-rain",
+      "heavy-rain" => "heavy-rain",
+      "flurries" => "flurries",
+      "light-snow" => "light-snow",
+      "heavy-snow" => "heavy-snow",
+      "very-light-sleet" => "light-sleet",
+      "light-sleet" => "light-sleet",
+      "heavy-sleet" => "heavy-sleet",
+      "breezy" => "breezy",
+      "dangerous-wind" => "dangerous-wind"
     }
     aliases[icon_name] || icon_name
   end
