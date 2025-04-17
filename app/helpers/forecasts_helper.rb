c88924d0db4c021318cfaf406f2876bd7ed46d79
ryan.weiss <rymiwe@gmail.com>
2025-04-16 21:06:08 -0700

    Add aliases for 'partly-cloudy-day' and 'partly-cloudy-night' to use 'cloudy-day' and 'cloudy-night' icons for Masters-Tempestacons compatibility.

--- app/helpers/forecasts_helper.rb
+++ app/helpers/forecasts_helper.rb
@@ -2,7 +2,9 @@ module ForecastsHelper
   def weather_icon_filename(icon_name)
     aliases = {
       "clear-day" => "day",
-      "clear-night" => "night"
+      "clear-night" => "night",
+      "partly-cloudy-day" => "cloudy-day",
+      "partly-cloudy-night" => "cloudy-night"
     }
     aliases[icon_name] || icon_name
   end
