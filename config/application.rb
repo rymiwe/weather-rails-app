c4f482406aa1f11d3624d246f54e07f6da7fb519
ryan.weiss <rymiwe@gmail.com>
2025-04-17 19:39:57 -0700

    Remove all remaining asset pipeline config references (assets.rb, application.rb) after propshaft removal

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module WeatherRailsApp
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w[tasks])
 
     # Autoload app/constants for custom constants like WeatherIconMap
     config.autoload_paths << Rails.root.join("app/constants")
