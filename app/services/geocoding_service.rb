83b1e45c49317f187f7bcd699cf95713b9690063
ryan.weiss <rymiwe@gmail.com>
2025-04-17 22:55:07 -0700

    Rubocop

--- app/services/geocoding_service.rb
+++ app/services/geocoding_service.rb
@@ -27,22 +27,22 @@ class GeocodingService
   unless lat && lon
     return nil
   end
-  
+
   # Prefer extracting from address hash if present
   address = geo_result.data["address"] || {}
   locality = address["city"] || address["town"] || address["village"] || address["suburb"] || address["neighbourhood"]
   state = address["state"]
   country = address["country"]
   location_name = [ locality, state, country ].compact.join(", ")
-  
+
   # FORCE 'us' units for any location in the US
   # Note: We check the query itself as well as the result in case the API doesn't return correct country codes
   us_query = query.to_s.match?(/\b(US|USA|Oregon|Washington|California|New York|Texas)\b/i)
   us_result = (country_code = geo_result.data["country_code"].to_s.upcase) == "US" || country.to_s.include?("United States")
-  
+
   # Simple rule: If it's a US location or query, use US units (Fahrenheit)
   units = (us_query || us_result) ? "us" : "si"
-  
+
   # DEBUGGING: Check what's happening with units detection
   Rails.logger.info "*** UNITS DETECTION ***"
   Rails.logger.info "Query: #{query}"
@@ -53,7 +53,7 @@ class GeocodingService
   Rails.logger.info "Is US query? #{us_query}"
   Rails.logger.info "Is US result? #{us_result}"
   Rails.logger.info "Using units: #{units}"
-  
+
   # Always log API calls in development for debugging
   if Rails.env.development?
     puts "\n*** GEOCODING RESULT ***"
